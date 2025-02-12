eca5e5e130fc0b64b1ba310cefc179915ac1771d
Harsh Parmar <kanh@Needle.local>
2025-02-12 12:32:02 -0500

    final working project

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Welcome to the product management app!');
+import "../scss/main.scss"; // Make sure the path is correct
+
+document.addEventListener("DOMContentLoaded", function () {
+	console.log("Welcome to the product management app!");
 });
